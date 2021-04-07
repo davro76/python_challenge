65c9b2c2f2ac0eb105423b86d52f3a2c7b0f7655
Rodney DAVERMANN <davermannrodney@gmail.com>
2021-04-06 23:27:43 -0400

    Python Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,45 +9,45 @@ Pybank = os.path.join("/Users/rodneydavermann/Desktop/python_challenge/PyBank/Re
 # Open and read the pybank.csv
 with open(Pybank) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=",")
+    
 
     # Read the header row first 
     csv_header = next(csv_file)
-    print(f"Header: {csv_header}")
 
     # initialize variables 
     count_row = 0
     total_amount_lossprofit = 0
     net_change = 0
+    first_loss_profit_value = 0
 
-    # create an empty list to hold the loss/profit column
+
+    # create empty lists to hold the loss/profit column and net change
     list_loss_profit = []
-
+    #net_change = []
+    
     # Read through each row of data after the header
     for row in csv_reader:
         count_row += 1
         loss_profit = row[1]
-
+        
         # append the lost/profit column to the list empty list and convert the elements from stringer to integer
         list_loss_profit.append(int(loss_profit))
 
         # total of loss/profit column
         sum_loss_profit = sum(list_loss_profit)
         total_amount_lossprofit = sum_loss_profit
-#print (total_amount_lossprofit)
+
 
         # net change: list_loss_profit minus previous change
-        previous_change = list_loss_profit[0]
-print(previous_change)
+        previousvalue = list_loss_profit[0]
+        net_change = int(row[1]) - previousvalue
 
-        #for i in list_loss_profit:
-            #list_loss_profit[i] = list_loss_profit - previous_change
-        #print(list_loss_profit)
+        # average net change
+        average_net_change = net_change / count_row
         
-#print(previous_change)
-
-        #total_loss_profit = loss_profit.
-#print(list_loss_profit)
-#size = len(list_loss_profit)
-#print(size)
       
-#print(count_row)
+#print calculated variables 
+print(count_row)
+print (total_amount_lossprofit)
+print(net_change)
+print(average_net_change)
